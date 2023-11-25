9fa12050522b25c91b9420eb26d5b4f70bb8f258
BenW07 <145677654+BenW07@users.noreply.github.com>
2023-11-24 17:52:32 -0800

    ASCII Assignment

---
+++ ASCII Assignment/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("  |----|                           _______             /------------\\");
+        System.out.println("  |    |                           |__       |__      /  /--------\\  |");
+        System.out.println("  |    |                             |___   ___|      | |          | | ");
+        System.out.println("  |____|                                |___|         |_|          | |");
+        System.out.println("                                                                   | |");
+        System.out.println("                                                            _______/ /");
+        System.out.println("       ___________________________________________          | ______/");
+        System.out.println("       |    |     |      |       |     |      ___/          | |");
+        System.out.println("       |    |     |      |       |     |   __/              | |");
+        System.out.println("       |    |     |      |       |     |  /                 |_|");
+        System.out.println("       ----------------------------------   ");
+        System.out.println("                                                            [_]");
+        System.out.println();
+        System.out.println("Computer Programming 11 - VLN");
+        System.out.println();
+        System.out.println("Benjamin Wosk");
+        System.out.println();
+        System.out.println("24 November 2023");
+    }
+}
